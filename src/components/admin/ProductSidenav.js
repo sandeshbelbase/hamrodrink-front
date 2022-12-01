194715d9480ee713062af94ffb7dc5d668b6c9d0
harishbohara11 <hb097854@gmail.com>
2022-12-01 15:08:34 +0545

    toggle

--- src/components/admin/ProductSidenav.js
+++ src/components/admin/ProductSidenav.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 const ProductSidenav = () => {
+    const [status, setStatus] = useState()
+    const handleAdd =()=>{
+        setStatus((prev)=> !prev)
+        console.log("sss", status)
+
+    }
     return(
         <div className="SideNav">
             <ul> 
                 <li><a href="./"> Home</a></li>
 
-                <li>
-                    <a href="./">Product</a>
-                    <ul>
+                <li onClick={handleAdd}>
+                    Product
+                    {
+                        status && <ul>
                         <li><a href="./">Add Product</a></li>
                         <li><a href="./">View Product</a></li>
                         <li><a href="./">Delete Product</a></li>
                     </ul>
+                    }
+                    
                 </li>
 
                 <li><a href="./"> Orders</a></li>
